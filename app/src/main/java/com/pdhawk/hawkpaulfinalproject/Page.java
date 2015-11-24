dd6e77cbabdd4669dc711bb24d97971b8c901ca6
PaulDHawk <hawkpaul@gmail.com>
2015-11-23 21:43:00 -0600

    first commit

---
+++ app/src/main/java/com/pdhawk/hawkpaulfinalproject/Page.java
@@ -0,0 +1,17 @@
+package com.pdhawk.hawkpaulfinalproject;
+
+
+public class Page {
+    public int id;
+    public int picture;
+    public int sound;
+
+    Page(int id, int picture, int sound ){
+        this.id = id;
+        this.picture = picture;
+        this.sound = sound;
+    }
+
+
+
+}
